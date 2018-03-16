d3d8a30763c3e0c974676cff6af29455c1bf1334
RepairWise <jdstapleton@verisk.com>
2018-03-16 11:58:27 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
 })
+
 export class AppComponent {
-  title = 'app';
+  public interval: Observable<number> = Observable.interval(500);
+  public task: Subscription;
+
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = true;
+  thumbLabel = false;
+  vertical = false;
+  _value = 1;
+  _step = 1;
+  _tickInterval = 1;
+
+  get value(): number {
+    return Math.round(this._value);
+  }
+  set value(v) {
+    if (this.task !== undefined) {
+      this.task.unsubscribe();
+    }
+    this.task = this.interval.subscribe(() => {
+      v = Math.round(Number(v));
+      this._value = v;
+    });
+  }
+  get step(): number {
+    return this._step;
+  }
+  set step(v) {
+    this._step = this.max / 100;
+  }
+  get tickInterval(): number | 'auto' {
+    return this._tickInterval;
+  }
+  set tickInterval(v) {
+    this._tickInterval = 100 / this.max;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { NgModule } from '@angular/core';
-
+// tslint:disable-next-line:max-line-length
+import { MatInputModule, MatFormFieldModule, MatSlider, MatCheckbox, MatCard, MatCardContent, MatRippleModule, GestureConfig } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatCard,
+    MatCardContent,
+    MatCheckbox,
+    MatSlider
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig },
+    FocusMonitor
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
